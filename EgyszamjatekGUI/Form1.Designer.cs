378cdc50f1688261ad5ba454940d1293dc77ea7b
ztibor <56896697+ztibor@users.noreply.github.com>
2022-12-16 23:57:03 +0100

    Add files via upload

---
+++ EgyszamjatekGUI/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace EgyszamjatekGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Játékos neve:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Játékos tippjei:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(138, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(329, 23);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(508, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(189, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Játékos hozzáadása";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 272);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "EgyszámjátékGUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EgyszamjatekGUI/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EgyszamjatekGUI
+{
+   
+    
+    public partial class Form1 : Form
+    {
+        public static List<Jatekos> lj;  
+
+        public Form1()
+        {
+            InitializeComponent();
+             
+    }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            int s = textBox2.Text.Trim().Split(' ').Length;
+            label3.Text = Convert.ToString(s) + " db";
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Jatekos> lj = new List<Jatekos>();
+            foreach (var k in File.ReadAllLines("egyszamjatek2.txt"))
+                lj.Add(new Jatekos(k));
+            int ldb = lj.Where(x => x.nev == textBox1.Text).ToList().Count;
+            int tippsz = lj[0].tipp.Count;
+            int tp = Convert.ToInt32(label3.Text.Substring(0, label3.Text.Length - 3));
+            if (ldb != 0)
+                MessageBox.Show("Van már ilyen nevű játékos","Hiba");
+            else if (tippsz != tp)
+                MessageBox.Show("Tippek száma nem megfelelő", "Hiba");
+            else
+            {
+                StreamWriter fs = File.AppendText("egyszamjatek2.txt");
+                string s = "";
+                s += textBox1.Text + " ";
+                foreach (var k in textBox2.Text.Trim().Split(' '))
+                    s += k + " ";
+                s = s.Trim()+"\n";
+                fs.Write(s);
+                fs.Close();
+                MessageBox.Show("Az állomány bővítése sikeres volt", "Üzenet");
+            }
+        }
+    }
+
+    public class Jatekos
+    {
+        public string nev;
+        public List<int> tipp;
+
+        public Jatekos(string sor)
+        {
+            string[] s = sor.Trim().Split(' ');
+            nev = s[0];
+            int db = s.Length - 1;
+            tipp = new List<int>();
+            foreach (var k in s.Skip(1))
+                tipp.Add(int.Parse(k));
+        }
+    }
+}
